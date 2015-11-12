29d38bc7bf66ba00bfc04ea8730766cd3c1d3a70
Oleksandr Turevskiy <oltur@ukr.net>
2015-11-12 15:45:39 +0200

    .

---
+++ web/tracks/3135013d-1294-447a-9410-37b2751bab00.js
@@ -0,0 +1,16 @@
+{
+"name":"killme2"
+,"isPublic":"No"
+,"travelWith":"Plane"
+,"copyright":"(c)"
+,"defaultScale":8
+,"photoLocationTolerancy":100
+,"numOfPhotos":20
+,"velocityMetersPerSec":21858
+,"icon":"myplane.png"
+,"trackImage":"a.gif"
+,"audioSrc":"germany.mp3"
+,"textToRead":"b.txt"
+,"trackData":[{"lat":50.69123805220047,"lng":31.289062500000004,"syt_text":""},{"lat":50.6947178381929,"lng":29.102783203125,"syt_text":""},{"lat":50.712112896185076,"lng":28.350219726562496,"syt_text":""},{"lat":50.65642599320274,"lng":27.5811767578125,"syt_text":""},{"lat":50.198001033269506,"lng":28.333740234375,"syt_text":""}]
+,"category":"Other"
+}
---
+++ web/tracks/789eead3-5232-48c6-a8bd-313593ee0b3f.js
@@ -0,0 +1,16 @@
+{
+"name":"New track 2015-10-29 15:17:44"
+,"isPublic":"No"
+,"travelWith":"Bus"
+,"copyright":"(c)"
+,"defaultScale":8
+,"photoLocationTolerancy":100
+,"numOfPhotos":20
+,"velocityMetersPerSec":100
+,"icon":"mybus.png"
+,"trackImage":"aman.gif"
+,"audioSrc":"germany.mp3"
+,"textToRead":""
+,"trackData":[[50.757309850294085,28.992919921875004,""],[50.21909462044748,29.366455078124996,""],[50.830228205617445,27.8173828125,""],[51.15867686442365,30.0860595703125,""]]
+,"category":"Other"
+}
